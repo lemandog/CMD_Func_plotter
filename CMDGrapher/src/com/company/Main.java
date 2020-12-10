afe788623bc6b801b43aec0d85ecd6e2479ede96
lemandog <73439281+lemandog@users.noreply.github.com>
2020-12-10 08:25:49 +0400

    Add files via upload

--- CMDGrapher/src/com/company/Main.java
+++ CMDGrapher/src/com/company/Main.java
@@ -2,37 +2,65 @@ package com.company;
 
 import java.util.Scanner;
 
+import static java.lang.Float.NaN;
+
 public class Main {
 
     public static void main(String[] args) {
-        int terminalWidth =  120;   //Нет способа понять, какого размера окно запущено, считаю что стандарт
-        int terminalHeight = 30;
+        int terminalWidth =  118;   //Нет способа понять, какого размера окно запущено, считаю что стандарт
+        int terminalHeight = 28;
 
         Scanner in = new Scanner(System.in);
-        System.out.println("Функция, в виде f(x), (например cos(x))");
-        String matheq = in.nextLine();
+        System.out.println("ГРАФОПОСТРОИТЕЛЬ");
+        System.out.println("1 - SIN");
+        System.out.println("2 - COS");
+        System.out.println("3 - TAN");
+        System.out.println("4 - ACOS");
+        System.out.println("5 - ASIN");
+        System.out.println("6 - EXP");
+        System.out.println("7 - SINH");
+        System.out.println("8 - COSH");
+        System.out.println("9 - TANH");
+        System.out.println("10 - LOG");
+        int type = in.nextInt();
         System.out.println("Начало счёта аргумента (Если 0, то -1)");
-        double xStart = in.nextInt();
+        double xStart = -Math.abs(in.nextInt());
+
         if (xStart == 0) {
             xStart = -1;
         }
-        System.out.println("Конец счёта аргумента (Если 0, то +1)");
-        double xEnd = in.nextInt();
-        if (xEnd == 0) {
-            xEnd = +1;
-        }
+
+        double xEnd = -xStart;
         System.out.println("Шаг (Если 0, то 0.001)");
         double deltaQuant = in.nextInt();
+
         if (deltaQuant == 0) {
             deltaQuant = 0.001;
+        }
+        if ((xEnd - xStart)/deltaQuant < 1) {
+            System.out.println("Слишком большой шаг, беру меньше!");
+            deltaQuant = (xEnd - xStart)/10000;
+            System.out.println("Шаг -" + deltaQuant);
         }
 
         //double[] result = (double[]) Parser (matheq,xEnd,xStart,deltaQuant);
         double[] result = new double[(int)((xEnd-xStart)/deltaQuant)];
         int counter = 0;
-
-        for (double i=xStart; i<xEnd; i=i+deltaQuant){
-            result[counter] = Math.sin(i);
+        if (type == 9) {xStart = deltaQuant;} //Защита от NaN
+        for (double i=xStart; i<xEnd-deltaQuant; i=i+deltaQuant){
+            switch (type) {
+                case 1: result[counter] = Math.sin(i); break;
+                case 2: result[counter] = Math.cos(i); break;
+                case 3: result[counter] = Math.tan(i); break;
+                case 4: result[counter] = Math.acos(i); break;
+                case 5: result[counter] = Math.asin(i); break;
+                case 6: result[counter] = Math.exp(i); break;
+                case 7: result[counter] = Math.sinh(i); break;
+                case 8: result[counter] = Math.cosh(i); break;
+                case 9: result[counter] = Math.tanh(i); break;
+                case 10: result[counter] = Math.log(i); break;
+                default: result[counter] = Math.sin(i); break;
+            }
             counter++;
         }
         OutputMethod(result,xEnd,xStart,deltaQuant,terminalWidth,terminalHeight,counter);
@@ -41,14 +69,15 @@ public class Main {
     public static void OutputMethod(double[] result, double xEnd, double xStart, double deltaQuant, int terminalWidth, int terminalHeight, int counter){
         //Высчитываем границы графопостроения
         double prevMax = result[0];
+
         for (int f=0; f<counter-1; f++){ //Поиск максимума функции
-            if (prevMax>result[f]){
-                prevMax = result[f];
+            if (prevMax<Math.abs(result[f]) && Math.abs(result[f]) != NaN ){
+                prevMax = Math.abs(result[f]);
             }
         }
-        double heightStep = Math.abs((prevMax*2)/terminalHeight); //Шаг по вертикали
-        double horisStep = Math.abs((xEnd-xStart)/terminalWidth); //Шаг по горизонтали
 
+        double heightStep = Math.abs((prevMax*2)/terminalHeight); //Шаг по вертикали
+        double horisStep = Math.abs((Math.abs(xEnd)+Math.abs(xStart))/terminalWidth); //Шаг по горизонтали
         //Форматирование исходных данных под терминал
         double currSum = 0;
         double[] resultAdapted = new double[terminalWidth];
@@ -61,26 +90,42 @@ public class Main {
             }
 
             resultAdapted[otr] = currSum/(amount); //Среднее для каждого знака
+            System.out.println(resultAdapted[otr] + "= Y, WITH X = " + (xStart + otr*horisStep));
         }
-        System.out.println(heightStep);
+
         System.out.println();
-        for (int h=0; h<terminalHeight; h++){ //Заполнить столбец
+        System.out.println(prevMax + " НАЙДЕННЫЙ ВЕРХНИЙ ПРЕДЕЛ");
+        System.out.println(heightStep+ "= ШАГ ГРАФОПОСТРОИТЕЛЯ ПО ВЕРТИКАЛИ");
+        System.out.println(horisStep + "= ШАГ ГРАФОПОСТРОИТЕЛЯ ПО ГОРИЗОНТАЛИ");
+
+        //Легенда
+        System.out.printf("%.1f",(float)xStart);
+        for (int w=0; w<terminalWidth-6; w++){//Заполнить строку
+            System.out.print(" ");
+        }
+        System.out.printf("%.1f",(float)xEnd);
+
+        System.out.println();
+        for (int w=0; w<terminalWidth; w++){//Заполнить строку
+            System.out.print("0");
+        }
+        System.out.println();
+        for (int h=terminalHeight; h>-1; h--){ //Заполнить столбец
             for (int w=0; w<terminalWidth; w++){ //Заполнить строку
-                if (h==terminalHeight-1 || h==0){
-                    System.out.print("X");
-                }
-                else{
-                     if (resultAdapted[w] > heightStep*h && resultAdapted[w] < heightStep*(h+1)){
-                           System.out.print("O");
+                if (resultAdapted[w] == NaN){resultAdapted[w]=0;} // Защита от NaN
+                     if (resultAdapted[w] > (-prevMax + heightStep*h) && resultAdapted[w] < (-prevMax + heightStep*(h+1)) ){
+                           System.out.print("0");
                       }
                       else {
                            System.out.print(" ");
-                            }
-                    }
+                      }
+
             }
             System.out.println(); //Перенос каретки (Строка заполнена)
     }
-
+        for (int w=0; w<terminalWidth; w++){//Заполнить строку
+            System.out.print("0");
+        }
     }
 
 
